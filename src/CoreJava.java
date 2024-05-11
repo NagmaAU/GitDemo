41d81c451d1686af8edf44c5aff4ee223819b69c
NagmaAU <nagma27.786@gmail.com>
2024-05-11 19:16:34 +0530

    first commit

---
+++ src/CoreJava.java
@@ -0,0 +1,18 @@
+public class CoreJava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num=5;
+		String name= "Nagma";
+		char letter='a';
+		double number=5.99;
+		boolean b=true;
+		
+		System.out.println(num);
+		
+		
+		
+
+	}
+
+}
